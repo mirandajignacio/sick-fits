c793e4a1b93e81c552c8ae35ab1a7587c1e328c4
Miranda J. Ignacio <mirandajignacio@gmail.com>
2019-01-12 21:19:03 -0300

    prisma connection

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const mutations = {};
+const mutations = {
+  createDog(parent, args, context, info) {
+    global.dogs = global.dogs || [];
+    //create a dog!
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    return newDog;
+  }
+};
 
 module.exports = mutations;
