f14c40218cbccc0c4db96b614088015eb86dc528
Miranda J. Ignacio <mirandajignacio@gmail.com>
2019-01-13 11:03:21 -0300

    item model graphql

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,10 @@
+const { forwardTo } = require("prisma-binding");
 const Query = {
-  dogs(parent, args, context, info) {
-    return [{ name: "Snickers" }, { name: "Summie" }];
-  }
+  items: forwardTo("db")
+  // async items(parent, args, context, info) {
+  //   const items = await context.db.query.items();
+  //   return items;
+  // }
 };
 
 module.exports = Query;
